4a9ef127455d5bf180407e32cd3e98735ddadd13
Joao Cesar dos Santos Pereira <64321827+mantecinfox@users.noreply.github.com>
2025-08-05 14:35:45 -0300

    Delete database/seeds/seed_users.js

--- database/seeds/seed_users.js
+++
@@ -1,8 +0,0 @@
-exports.seed = function(knex) {
-  return knex('users').del()
-    .then(function () {
-      return knex('users').insert([
-        { name: 'Admin', email: 'admin@example.com' }
-      ]);
-    });
-};
